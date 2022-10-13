e2b166c82326fc36535bafe42783e0fa09e1abc3
JorgeGaitanDev <jgaitanh12@gmail.com>
2022-10-13 17:21:07 -0500

    testfour

--- Sophos.js
+++ Sophos.js
@@ -1,26 +1,26 @@
 
 let hash = "3837ac82f7ff2dd216764ac90622166d";
-let s = hash.split("").find(x => !isNaN(x) && x > 0) ;
+let s = hash.split("").find(x => !isNaN(x)) ;
 
-//point zero
+//challenge 0
 console.log('3837ac82f7ff2dd216764ac90622166d -> ' + s);
 
-//point one
+//challenge 1
 function deleteSValue(array) {
     let arrayStr = array.reverse().map( x => {return x.toString().split("")}).flat();
     let intArray = arrayStr.map( x => {return parseInt(x)});
     return intArray.filter( x => {return x < s});
 }
 
-// console.log(deleteSValue([1,2,3,4,5,6]));// [ 2, 1 ]
-// console.log(deleteSValue([3]));// []
-// console.log(deleteSValue([33]));// []
-// console.log(deleteSValue([32]));// [ 2 ]
-// console.log(deleteSValue([3,2,1]));// [ 1, 2 ]
-// console.log(deleteSValue([30, 6, 5, 4, 3, 2, 7, 7, 29, 1]));
-// console.log(deleteSValue([10, 20, 30, 40]));
+console.log(deleteSValue([1,2,3,4,5,6]));// [ 2, 1 ]
+console.log(deleteSValue([3]));// []
+console.log(deleteSValue([33]));// []
+console.log(deleteSValue([32]));// [ 2 ]
+console.log(deleteSValue([3,2,1]));// [ 1, 2 ]
+console.log(deleteSValue([30, 6, 5, 4, 3, 2, 7, 7, 29, 1]));
+//console.log(deleteSValue([10, 20, 30, 40]));
 
-//point two
+//Challenge 2
 function orderArray(array){
     let intS = parseInt(s);
     let absValor = array.map(x => {return Math.abs(x)}).sort();
@@ -28,52 +28,23 @@ function orderArray(array){
     return potValor;
 }
 
-// console.log(orderArray([1, 2, 3, 5, 6, 8, 9]));//[1, 4, 9, 25 ]
-// console.log(orderArray([-2, -1]));// [1, 4])
-// console.log(orderArray([-6, -5, 0, 5, 6]));// [0, 25, 25 ]
-// console.log(orderArray([-10, 10]));// []
+console.log(orderArray([1, 2, 3, 5, 6, 8, 9]));//[1, 4, 9, 25 ]
+console.log(orderArray([-2, -1]));// [1, 4])
+console.log(orderArray([-6, -5, 0, 5, 6]));// [0, 25, 25 ]
+console.log(orderArray([-10, 10]));// []
 
-//point three
-function Coin(array) {
-    
-    array.sort();
-    let cambio = 0;
-    let  array.forEach(element => array {
-            let changeToBeCreated = changeYaCreated + 1;
-            if (item > changeToBeCreated) {
-                return changeToBeCreated;
-                cambio += element;
-            }
-            return cambio + 1;
-        });
-    // private int NonConstructibleChange(List<int> monedas) 
-    // { 
-    //    // Complejidad de tiempo - O(n logn)
-    //     // Complejidad de espacio - O(1)
-       
-    //     monedas.Sort(); 
-    //    int cambioYaCreado = 0; 
-    //    foreach (elemento var en monedas) 
-    //    { 
-    //       int changeToBeCreated = changeYaCreated + 1; 
-    //       if (item > changeToBeCreated) 
-    //             return changeToBeCreated ; 
-    //       cambioYaCreado += elemento; 
-    //    } 
-       
-    //    // si la matriz está vacía, 
-    //    devuelve cambioYaCreado + 1; 
-    // }
-  
-  
-  
-  
-    // array.sort();
-    // let ncc = 1;
-    // for (i=0 ; i < array.length && array[i]<=ncc; i++){
-    //     ncc +=array[i]
-    // }
-    // return ncc;
+//Challengue 3
+function Coin(array=[]) {
+array.sort((x,y)=>x-y);
+let curChange = 0;
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (element > curChange + 1) {
+        return curChange + 1;
+    }
+    curChange += element;
+}
+return curChange + 1;
 }
 console.log(Coin( [5, 7, 1, 1, 2, 3, 22]));// 20
 console.log(Coin([1, 1, 1, 1, 1]));// 6
